84b4f023a51bfb29500a0cefc49909d6c92d29b0
Allan Melo <allmelo@users.noreply.github.com>
2020-09-16 21:21:31 -0300

    Styling Header & Tabs

---
+++ src/components/Tabs/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {Container, TabsContainer, TabItem, TabText} from './styles';
+
+const Tabs = () => {
+  return (
+    <Container>
+      <TabsContainer>
+        <TabItem>
+          <Icon name="person-add" size={24} color="#FFF" />
+          <TabText>Indicar amigos</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="person-add" size={24} color="#FFF" />
+          <TabText>Indicar amigos</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="person-add" size={24} color="#FFF" />
+          <TabText>Indicar amigos</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="person-add" size={24} color="#FFF" />
+          <TabText>Indicar amigos</TabText>
+        </TabItem>
+        <TabItem>
+          <Icon name="person-add" size={24} color="#FFF" />
+          <TabText>Indicar amigos</TabText>
+        </TabItem>
+      </TabsContainer>
+    </Container>
+  );
+};
+
+export default Tabs;
